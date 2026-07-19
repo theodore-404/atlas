4e2aa0b177122072b3eb398e6eab0ab9a1b43f2b
theodore-404 <tioonlygame@gmail.com>
2026-07-19 22:07:13 +0700

    Create mission2.py

---
+++ atlas-academy/mission2.py
@@ -0,0 +1,8 @@
+print("=== ATLAS Access Control ===")
+name = input("Enter your name: ")
+if name == "Wynn":
+    print("Access Granted.")
+    print("Welcome, Commander.")
+else:
+    print("Access Denied.")
+    print("Unknown User.")
